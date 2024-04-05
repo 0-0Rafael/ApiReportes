a2100ae4a3df3bb3958975e54991c7f9d1a41463
Baljeet <rafaelarielcg@gmail.com>
2024-04-05 09:11:04 -0400

    Add nodos on enum

--- src/main/java/com/example/rest/domain/Nodos/Nodo.java
+++ src/main/java/com/example/rest/domain/Nodos/Nodo.java
@@ -1,7 +1,13 @@
 package com.example.rest.domain.Nodos;
 
 public enum Nodo {
+    LOS_MINA,
+    CCE,
     HAINAMOSA,
+    HERRERA_NUEVA,
+    SAN_PEDRO1,
+    SAN_PEDRO2,
+
     GALLERA,
-    PUERTO_PLATA
+    PUERTO_PLATA,
 }
