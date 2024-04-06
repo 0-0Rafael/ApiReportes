f4c0b204dda76d36b57a62abc63c66da614a71fd
Baljeet <rafaelarielcg@gmail.com>
2024-04-06 07:57:14 -0400

    CHANCE POM for add new dependency, CHANCE MAINAPPLICATION FOR CLASS TOMCAT CONFIGURATION

--- src/main/java/com/example/rest/Reporteapi17ofApplication.java
+++ src/main/java/com/example/rest/Reporteapi17ofApplication.java
@@ -2,14 +2,21 @@ package com.example.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Reporteapi17ofApplication {
+public class Reporteapi17ofApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Reporteapi17ofApplication.class, args);
 		System.out.println("Api de Reportes Corriendo");
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(Reporteapi17ofApplication.class);
 	}
 
 }
